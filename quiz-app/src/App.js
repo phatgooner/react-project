f216b5372a67a6b9df6142ff03e1fd2068ab57f4
Phat Gooner <nguyentienphat1604@gmail.com>
2025-07-08 14:24:20 +0700

    update header

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app-container">
+      <Header />
       <p>
         Hello world
       </p>
